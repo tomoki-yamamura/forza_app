396c0eddb079c5a41e69cf4db105212ca7be6844
tomoki-yamamura <progtab17@gmail.com>
2021-06-18 00:54:08 +0900

    210617

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/help', to: 'forza#help'
   get '/blog', to: 'forza#blog'
   get '/contact', to: 'forza#contact'
+  get '/about', to: 'forza#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
