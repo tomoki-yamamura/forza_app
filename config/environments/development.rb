cfc4307f0be5448b5680668086d96db854b3a855
tomoki-yamamura <progtab17@gmail.com>
2021-09-14 23:38:16 +0900

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
    address:              'smtp.gmail.com',
    port:                  587,
    domain:               'gmail.com',
-   user_name:             ENV["MAIL_USERNAME"],
-   password:              ENV["MAIL_PASSWORD"],
+   user_name:             ENV['MAIL_USERNAME'],
+   password:              ENV['FORZA_PASSWORD'],
    authentication:       'plain',
    enable_starttls_auto:  true
   }
