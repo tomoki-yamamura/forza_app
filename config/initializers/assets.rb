8aab07563701269ecc0550cd3b58eedaf3b2486e
tomoki-yamamura <progtab17@gmail.com>
2021-09-23 23:22:08 +0900

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,3 +16,19 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w(script/*.js)
 
 Rails.application.config.assets.precompile += %w( scroll-reveal.js )
+
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    if full_path.starts_with? app_assets_path
+      logger.info "including asset: " + full_path
+      true
+    else
+      logger.info "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+end
