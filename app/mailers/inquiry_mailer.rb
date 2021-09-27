4970e015f3aa9dd020abf5c612085111231afd17
tomoki-yamamura <progtab17@gmail.com>
2021-09-27 22:21:46 +0900

    favicon

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -3,8 +3,8 @@ class InquiryMailer < ApplicationMailer
   def send_mail(inquiry)
     @inquiry = inquiry
     mail(
-      from: "forza.kanazawa.volleyball@gmail.com",
-      to:   "forza.kanazawa.volleyball@gmail.com",
+      from: ENV["FORZA_EMAIL"],
+      to:   ENV["FORZA_EMAIL"],
       subject: 'お問い合わせ通知'
     )
   end
